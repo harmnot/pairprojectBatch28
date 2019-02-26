14d52db3f14c694f045f1e73f923ad981af89ee6
Muhammad <myhoomac@hotmail.com>
2019-02-27 00:58:28 +0700

    tugas pair project

--- app.js
+++ app.js
@@ -4,15 +4,36 @@ const app = express();
 import expressLayouts from "express-ejs-layouts";
 import ejs from "ejs";
 
-const PORT = process.env.PORT || 3000;
+import flash from "connect-flash";
+import session from "express-session"
 
-
+// EJS
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true})););
+app.use(express.urlencoded({ extended: true }));
 
+// express-session
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
+// connect flash
+app.use(flash());
+
+// global variables for flash
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`you are connected at port ${PORT}`));
